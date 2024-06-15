5a322679516ab55a1e95066b241932ff273b6e52
igomez <ignacio.gomez.oliva@alumnos.upm.es>
2024-06-15 11:04:46 +0200

    Compiling contract

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,10 @@
 const Migrations = artifacts.require('Migrations');
+const TFG = artifacts.require('TFG');
+const Market = artifacts.require('Market');
 
 module.exports = function (deployer) {
 	deployer.deploy(Migrations);
+	deployer.deploy(TFG);
+	deployer.deploy(Market);
+
 };
--- src/contracts/TFG.sol
+++ src/contracts/TFG.sol
@@ -1,63 +1,43 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
 
-contract RoyaltyNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, ERC2981 {
-    using Counters for Counters.Counter;
+contract TFG is ERC721, ERC721URIStorage, Ownable, ERC2981 {
 
-    Counters.Counter private _tokenIdCounter;
+    uint256 private _tokenIdCounter = 0;
 
-    constructor() ERC721("Non Fungible Notes", "NFN") {
+    constructor(address initialOwner)
+        ERC721("Non Fungible Notes", "NFN")
+        Ownable(initialOwner)
+    {
         _setDefaultRoyalty(msg.sender, 100);
     }
 
-    function mintNFT(address recipient, string memory tokenURI)
+    function safeMint(address to, string memory uri) 
         public onlyOwner
-        returns (uint256) 
-    {
-        _tokenIdCounter.increment();
-
-        uint256 newItemId = _tokenIdCounter.current();
-        _safeMint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-
-        return newItemId;
+        returns(uint256) {
+        uint256 tokenId = _tokenIdCounter++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        return tokenId;
     }
 
-    function mintNFTWithRoyalty(address recipient, string memory tokenURI, address royaltyReceiver, uint96 feeNumerator)
+
+    function mintNFTWithRoyalty(address recipient, string memory uri, address royaltyReceiver, uint96 feeNumerator)
         public onlyOwner
         returns (uint256) 
     {
-        uint256 tokenId = mintNFT(recipient, tokenURI);
+        uint256 tokenId = safeMint(recipient, uri);
         _setTokenRoyalty(tokenId, royaltyReceiver, feeNumerator);
 
         return tokenId;
     }
-    }
 
     // The following functions are overrides required by Solidity.
-
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-        _resetTokenRoyalty(tokenId);
-    }
-
-    function burnNFT(uint256 tokenId)
-        public onlyOwner {
-        _burn(tokenId);
-    }
 
     function tokenURI(uint256 tokenId)
         public
@@ -71,19 +51,10 @@ contract RoyaltyNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, ERC2
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable, ERC2981)
+        override(ERC721, ERC721URIStorage, ERC2981)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
-    }
-
-    function _beforeConsecutiveTokenTransfer(
-        address,
-        address,
-        uint256,
-        uint96
-    ) internal virtual override(ERC721, ERC721Enumerable) {
-        revert("ERC721Enumerable: consecutive transfers not supported");
     }
 
 }
